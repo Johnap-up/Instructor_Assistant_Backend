e1de3ae4abdcfc057d3ce19bc43e56209a078881
tukumi <tukumij@gmail.com>
2024-03-21 14:05:26 +0800

    first

---
+++ src/main/java/org/example/InstructorAssistantBackApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstructorAssistantBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstructorAssistantBackApplication.class, args);
+    }
+
+}
